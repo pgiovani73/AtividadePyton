462b42010cedfce01923a5c69e1df69ae07d3994
pgiovani73 <pgiovani.teixeira@gmail.com>
2024-10-10 17:42:58 -0400

    aula 10out

---
+++ extrato-1010.py
@@ -0,0 +1,38 @@
+print("|========= BANCO DO BRASIL =========|\n")
+user=int(input("Digite tua Senha: "))
+saldo=250
+if user==4321:
+     print("\n|---------MENU--------|")
+     print("|    1 - Extrato      |\n""|    2 - Deposito     |\n""|    3 - Saque        |\n""|    4 - Adm          |\n""|    5 - Sair         |")
+     print("|---------------------|")
+     op=int(input(" Digite a opção:"))
+     if op==1:
+         print(" R$",saldo)
+     elif op==2:
+         deposito=float(input("\n Digite o valor do deposito:R$ "))
+         saldo=saldo+deposito
+         print(" Valor de Saldo:R$",saldo)
+     elif op==3:
+         saque=float(input("\n Digite o valor do saque:R$"))
+         saldo=saldo-saque
+         print(" Valor de Saldo:R$",saldo)
+     elif op==4:
+        print("\n|---------------------|""\n|  1-Cadastro         |""\n|  2-Editar Cadastro  |""\n|---------------------|\n")
+        cadastro=int(input("Escolha a opção:"))
+        if cadastro==1:
+            print("\nNome: Pedro Giovani Teixeira")
+            print("CPF:  582725691-91")
+            print("Fone: 67998271385")
+            print("sexo: Masculino")
+        elif cadastro==2:
+            nome=(input("Digite o nome: "))
+            cpf=(input("Digite o CPF:"))
+            fone=(input("Digite o fone:"))
+            sexo=(input("Digite o sexo:"))
+            print(nome,"\n",cpf,"\n",fone,"\n",sexo)
+     elif op==5:
+        print("|--------TERMINO DA SESSÃO--------|")
+     else:
+        print("|----------OPÇÃO INVALIDA---------|")
+else:
+    print(":( :( ACESO NEGADO :( :(")
