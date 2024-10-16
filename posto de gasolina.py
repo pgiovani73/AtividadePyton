ba50a64a48b486bdd394455e8016404bb373d3a8
pgiovani73 <pgiovani.teixeira@gmail.com>
2024-10-16 15:01:17 -0400

    rsolvendo o algoritmo
    resolvendo o algoritmo que nao terminei nessa aula

--- posto de gasolina.py
+++ posto de gasolina.py
@@ -1,7 +1,22 @@
-combustivel=float("Quantidade de combustivel:")
-a=1.90
-g=2.50
-alcool=a*combustivel
-gasolina=g*combustivel
-if combustivel ==a:
-    if a>=20
+litros = float(input("Digite a quantidade de litros abastecidos: "))
+tipo_combustivel = input("Digite o tipo de combustível (A para álcool, G para gasolina): ")
+
+preco_alcool = 1.90
+preco_gasolina = 2.50
+
+if tipo_combustivel.upper() == 'A':
+        if litros <= 20:
+            desconto = 0.03
+        else:
+            desconto = 0.05
+            preco_litro = preco_alcool * (1 - desconto)
+elif tipo_combustivel.upper() == 'G':
+        if litros <= 20:
+            desconto = 0.04
+        else:
+            desconto = 0.06
+            preco_litro = preco_gasolina * (1 - desconto)
+
+valor_a_pagar = litros * preco_litro
+
+print(f"O valor total a ser pago é: R$ {valor_a_pagar:.2f}")
