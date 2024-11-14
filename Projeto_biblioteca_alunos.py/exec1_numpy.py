d43b954ebf221c56fce9251ef3e05b6eb4fae3de
pgiovani73 <pgiovani.teixeira@gmail.com>
2024-11-14 17:33:32 -0400

    numpy
    desenvolvendo exercicio sobre a biblioteca numpy

--- Projeto_biblioteca_alunos.py/exec1_numpy.py
+++ Projeto_biblioteca_alunos.py/exec1_numpy.py
@@ -1,5 +1,25 @@
-# calcular media e desvio padrao
+# criar array uma função de array aleatorio com 5 linhas e 2 colunas
 import numpy as np
+array_aleatorio=np.random.rand(10).reshape(5,2)
+print("array aleatorio", array_aleatorio,"\n")
 
-media=np.mean(a=10)
-desvio_padrao=np.std(a=10)
+#crair uma matriz dimensional com os seguinte numeros abaixo e depois soma a matriz;
+#x= 10,21,32,4,5 e 4,57,65,41,10
+x=np.array([[1,2,3,4,5], [4,57,65,41,10]])
+print(x)
+print(x.sum())
+print(x.mean())
+print(x.max()) 
+print(x.min(),"\n")
+#m edia=np.mean(array) 
+# soma=np.sum(array)
+# valor maximo da matriz=np.max(array) e valor minmo = np.min(array)
+
+
+#Criar um array 3D com 3 camadas, cada uma com 3 dimensões, 
+a=np.arange(27).reshape(3,3,3)
+print(a)
+print(a.ndim) # indica numeros de dimensoes e camadas
+
+matriz=array_aleatorio.resahpe(5,2)
+print(matriz)
